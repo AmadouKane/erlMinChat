c7aae377f8793d7d757e3f342350b38393d07076
Amadou Kane <aamadu.kane@gmail.com>
2020-02-05 22:22:44 +0000

    Amélioration!

--- src/base.erl
+++ src/base.erl
@@ -4,20 +4,31 @@
 
 -module(base). 
 -author('Amadou KANE'). 
-
+-export([start/1, accueil/1]).
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([ecoute/1]). 
+start(Port) ->
+    controleur:start(),
+    serveur_tcp:start(?MODULE, Port, {?MODULE, accueil}).
 
--define(OPTIONS_TCP, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]). 
-
-% Pour démarrer le service appeler la fonction base:ecoute(Port)
-ecoute(Port) ->
-	{ok, LSocket} = gen_tcp:listen(Port, ?OPTIONS_TCP),
-	accept(LSocket). 
-
-
+accueil(Socket) ->
+    gen_tcp:send(Socket, "\n********BIENVENU DANS MiNiMaL CHaT*******\n***********************************************\n"),
+    gen_tcp:send(Socket, "TAPER: CHAT, POUR DEMMARER!\n"),
+    case gen_tcp:recv(Socket, 0) of
+        {ok, Data} ->
+            io:format("Data: ~p~n", [binary_to_list(Data)]),
+            MessageDeDemmarage = binary_to_list(Data),           
+            case MessageDeDemmarage of
+                "CHAT" ->
+                    try_connection(pick_nickname(), Socket);
+                _ ->
+                    gen_tcp:send(Socket, "VOUS DEVEZ SAISIR: CHAT, POUR DEMMARER!\n"),
+                    ok
+            end;
+        {error, closed} ->
+            ok
+    end.
 
 
 
@@ -29,23 +40,67 @@ ecoute(Port) ->
 % 
 %Attend les connexions entrantes pour lancer base:loop/1
 %
-accept(LSocket) ->
-	{ok, Socket} = gen_tcp:accept(LSocket),
-	spawn(fun() -> loop(Socket) end),
-	accept(LSocket). 
+try_connection(Nick, Socket) ->
+    Response = gen_server:call(controller, {connect, Nick, Socket}),
+    case Response of
+        {ok, List, NouveauMessages} ->
+            gen_tcp:send(Socket, "VOUS AVEZ REJOINT LE CHAT AVEC SUCCES :" ++ List ++ "\n"),
+            %affiche les messages éhangé
+            gen_tcp:send(Socket, "-------------------ICI LES MESSAGES ECHANGES-------------------------\n" ++ NouveauMessages ++ "\n"),
+            gen_server:cast(controller, {join, Nick}),
+            loop(Nick, Socket);
+        nick_in_use ->
+            gen_tcp:send(Socket, "CONNECT:ERROR:NOM DEJA PRIS.\n"),
+            ok
+    end.
 
-%
-% Affiche les informations saisies
-%
-loop(Socket) ->
-	%reception ds données de la socket
-	case gen_tcp:recv(Socket, 0) of
-		{ok, Data} ->
-			%affichage
-			gen_tcp:send(Socket, Data),
-			loop(Socket);
-		{error, closed} ->
-			ok
-	end. 
 
+loop(Nick, Socket) ->
+    case gen_tcp:recv(Socket, 0) of
+        {ok, Data} ->
+            io:format("Data: ~p~n", [binary_to_list(Data)]),
+            Message = binary_to_list(Data),
+            {Command, [_|Content]} = lists:splitwith(fun(T) -> [T] =/= ":" end, Message),
+            case Command of
+                "SAY" ->
+                    say(Nick, Socket, clean(Content));                
+                "QUIT" ->
+                    quit(Nick, Socket)
+            end;
+        {error, closed} ->
+            ok
+    end.  
+
+say(Nick, Socket, Content) ->
+    gen_server:cast(controller, {say, Nick, Content}),
+    loop(Nick, Socket).
+
+quit(Nick, Socket) ->
+    Response = gen_server:call(controller, {disconnect, Nick}),
+    case Response of
+        ok ->
+            gen_tcp:send(Socket, "--------------------Bye!------------------\n"),
+            gen_server:cast(controller, {left, Nick}),
+            ok;
+        user_not_found ->
+            gen_tcp:send(Socket, "---------------------Error, Bye!-----------\n"),
+            ok
+    end.
+
+pick_nickname() ->
+lists:nth(random:uniform(12), firstnames())
+++ " " ++
+lists:nth(random:uniform(12), lastnames()).
+ 
+firstnames() ->
+["Amadou", "Salif", "Valerie", "Arnold", "Carlos", "Dorothy", "Keesha",
+"Phoebe", "Ralphie", "Tim", "Wanda", "Janet"].
+ 
+lastnames() ->
+["Kane", "Ba", "Frizzle", "Perlstein", "Ramon", "Ann", "Franklin",
+"Terese", "Tennelli", "Jamal", "Li", "Perlstein"].
+
+
+clean(Data) ->
+    string:strip(Data, both, $\n).
 
--- src/serveur_tcp.erl
+++ src/serveur_tcp.erl
@@ -2,7 +2,7 @@
 %% @doc @todo Add description to serveur_tcp.
 
 
--module(sereur_tcp).
+-module(serveur_tcp).
 
 -behavior(gen_server).
 
@@ -20,12 +20,39 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([]).
+start(Name, Port, Loop) ->
+    State = #server_state{port = Port, loop = Loop},
+    gen_server:start_link({local, Name}, ?MODULE, State, []).
 
+init(State = #server_state{port=Port}) ->
+    case gen_tcp:listen(Port, ?TCP_OPTIONS) of
+        {ok, LSocket} ->
+            NewState = State#server_state{lsocket = LSocket},
+            {ok, accept(NewState)};
+        {error, Reason} ->
+            {stop, Reason}
+    end.
+
+handle_cast({accepted, _Pid}, State=#server_state{}) ->
+    {noreply, accept(State)}.
 
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
+accept_loop({Server, LSocket, {Module, LoopFunction}}) ->
+    {ok, Socket} = gen_tcp:accept(LSocket),
+    gen_server:cast(Server, {accepted, self()}),
+    Module:LoopFunction(Socket).
+
+accept(State = #server_state{lsocket=LSocket, loop = Loop}) ->
+    proc_lib:spawn(?MODULE, accept_loop, [{self(), LSocket, Loop}]),
+    State.
+
+% These are just here to suppress warnings.
+handle_call(_Msg, _Caller, State) -> {noreply, State}.
+handle_info(_Msg, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVersion, State, _Extra) -> {ok, State}.
 
 
